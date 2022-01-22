b4a475b8d983d6334788c003d332ec16350aa1f4
Paola Huyo <di.paolahuyo@gmail.com>
2022-01-22 18:22:22 -0500

    agregando funcion evaluacion del input de tarjeta de credito

--- src/index.js
+++ src/index.js
@@ -5,14 +5,24 @@ let creditCardNumber = document.getElementById("numberTc");
 let buttonTc = document.getElementById("buttonTc");
 let contenedorTarjeta = document.getElementById("contenedorTarjeta");
 
+//Función que evalua si el input del usuario es un numero de 16 caracteres
+/*function esNumero
 
 
+
+*/
+
+
+//Guarda la variable del numero de tarjeta en el click y realiza la validación en el click
 buttonTc.addEventListener("click", () => {
     let resultado = validator(numberTc.value);
 contenedorTarjeta.innerText = "La tarjeta a validar es " + resultado;
 
 console.log("resultado",resultado);
-
 }
 );
+
+
+
+
 
--- src/validator.js
+++ src/validator.js
@@ -29,5 +29,4 @@ function isValid() {
     }
 };
 
-
 export default validator;
